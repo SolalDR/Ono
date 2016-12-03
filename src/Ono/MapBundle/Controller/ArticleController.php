261ee6473c997d49e98c866bb727f38c3f71fd84
SolalDR <solal.dussout-revel@hotmail.fr>
2016-12-03 19:53:45 +0100

    Add articles to Admin pannel

--- src/Ono/MapBundle/Controller/ArticleController.php
+++ src/Ono/MapBundle/Controller/ArticleController.php
@@ -23,12 +23,20 @@ use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class ArticleController extends Controller
 {
-  public function indexAction(){
+  public function indexAction(Request $request){
     $manager = $this->getDoctrine()->getManager();
     $repoArticle = $manager->getRepository("OnoMapBundle:Article");
     $themesRepo = $manager->getRepository("OnoMapBundle:Theme");
     $themes = $themesRepo->findAll();
     $articles = $repoArticle->findAll();
+
+    $routeName = $request->get('_route');
+    if($routeName === "ono_admin_list_articles") {
+      return $this->render('OnoMapBundle:Admin:list-articles.html.twig', array(
+        "articles" => $articles
+      ));
+    }
+
     return $this->render("OnoMapBundle:Article:index.html.twig", array(
       "articles" => $articles,
       "themes" => $themes
@@ -80,6 +88,14 @@ class ArticleController extends Controller
           ));
       }
 
+      $routeName = $request->get('_route');
+      if($routeName === "ono_admin_add_article") {
+        return $this->render('OnoMapBundle:Admin:add-article.html.twig', array(
+          "article" => $article,
+          "form" => $form->createView()
+        ));
+      }
+
       return $this->render('OnoMapBundle:Article:add.html.twig', array(
         'form' => $form->createView(),
         "themes" => $themes
@@ -103,6 +119,13 @@ class ArticleController extends Controller
     if($article && $user instanceof User && $user->getId() === $article->getUser()->getId()){
       if($this->container->get('security.authorization_checker')->isGranted('ROLE_EDITOR')){
         $form = $this->get('form.factory')->create(ArticleType::class, $article);
+        $routeName = $request->get('_route');
+        if($routeName === "ono_admin_edit_article") {
+          return $this->render('OnoMapBundle:Admin:edit-article.html.twig', array(
+            "article" => $article,
+            "form" => $form->createView()
+          ));
+        }
         return $this->render('OnoMapBundle:Article:edit.html.twig', array(
           'form' => $form->createView(),
           "themes" => $themes,
@@ -121,6 +144,7 @@ class ArticleController extends Controller
         ));
       }
     }
+
     return $this->redirectToRoute("ono_map_article_view", array(
       "id" => $numId
     ));
@@ -181,5 +205,39 @@ class ArticleController extends Controller
     }
     //L'utilisateur n'est pas authentifié
     return $this->redirectToRoute('ono_map_homepage');
+  }
+
+  public function deleteAction(Request $request)
+  {
+      $numId = (int) $request->attributes->all()["id"];
+      $manager = $this->getDoctrine()->getManager();
+
+      // On récupère l'annonce $numId
+      $article = $manager->getRepository('OnoMapBundle:Article')->find($numId);
+
+      if (null === $article) {
+        throw new NotFoundHttpException("Le pays d'id ".$numId." n'existe pas.");
+      }
+
+      // On crée un formulaire vide, qui ne contiendra que le champ CSRF
+      // Cela permet de protéger la suppression d'annonce contre cette faille
+      $form = $this->createFormBuilder()->getForm();
+
+      if ($form->handleRequest($request)->isValid()) {
+        $manager->remove($article);
+        $manager->flush();
+
+        $request->getSession()->getFlashBag()->add('info', "Le pays a bien été supprimée.");
+
+        return $this->redirect($this->generateUrl('ono_admin_list_articles'));
+      }
+
+      // Si la requête est en GET, on affiche une page de confirmation avant de supprimer
+      return $this->render('OnoMapBundle:Admin:delete.html.twig', array(
+        'object' => $article,
+        'title' => $article->getTitle(),
+        'pathDelete' => "ono_admin_delete_article",
+        'form'   => $form->createView()
+      ));
   }
 }
